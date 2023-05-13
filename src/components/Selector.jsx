b686a755c335ba765be74081a4bb21fc5feb3698
Dumebi Okolo <okolodumebi@gmail.com>
2023-05-13 22:16:54 +0100

    work on button

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -3,36 +3,41 @@ import '../index.css';
 
 export default function Selector() {
    return(
-    <div className="background">
-      <h1>
+    <div className="background flex-row">
+      {/* <h1>
         We are trying, to the absolute glory of our Lord Jesus!!
-      </h1>
-      <div className='flex mb-7'>
-        <button className='rounded-full mr-5 ml-5'>
-            1
+      </h1> */}
+      <div className='flex-col'>
+      <div className='flex-container'>
+        <button className=' mr-5 ml-5 '>
+            aaaaaa
         </button>
         <p> Step 1 <br/> <span className='mt-8'>YOUR INFO</span></p>
+
       </div>
-      <div className='flex mb-7'>
-        <button className='rounded-full mr-5 ml-5'>
+      <div className='flex-container'>
+        <button className='mr-5 ml-5  '>
             2
         </button>
         <p> Step 2 <br/> <span>SELECT PLAN</span></p>
-      </div>
-      <div className='flex mb-7'>
-        <button className='rounded-full mr-5 ml-5'>
-            3
-        </button>
-        <p> Step 3 <br/> <span>ADD-ONS</span></p>
-      </div>
-      <div className='flex mb-7'>
-        <button className='rounded-full mr-5 ml-5'>
-            4
-        </button>
-        <p> Step 4 <br/> <span>SUMMARY</span></p>
-      </div>
-    </div>
 
+      </div>
+        <div className='flex-container'>
+          <button className='mr-5 ml-5  '>
+             3
+          </button>
+          <p> Step 3 <br/> <span>ADD-ONS</span></p>
+
+        </div>
+        <div className='flex-container'>
+           <button className='mr-5 ml-5  '>
+             4 
+          </button>
+          <p> Step 4 <br/> <span>SUMMARY</span></p>
+
+        </div>
+      </div>     
+      </div>
    )
 }
 
