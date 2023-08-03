02f038f7caec4c10eea81e6e16544b63c2ddba4b
Dumebi Okolo <okolodumebi@gmail.com>
2023-08-03 01:08:03 +0100

    final touches

---
+++ src/components/finishingUp2.jsx
@@ -0,0 +1,74 @@
+import Selector from "./Selector";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function FinishingUp2() {
+     
+
+    
+  return (
+    <div className=" bg-white mt-10 ml-[11rem] white-rounding w-[61rem]">
+    <div className="flex">
+      <div className="m-3">
+        <Selector
+         />
+      </div>
+      <main className="ml-20 mt-10">
+      <div>
+        <h1 className="text-3xl text-blue-900 font-bold">Finishing up</h1>
+        <p className="paragraph font-normal text-sm mt-1">
+          Double check everything before confirming
+        </p>
+      </div>
+      <div className="bg-blue-100 w-[25rem] ml-1 mt-2">
+        <div className="flex m-3 space-x-[13rem] text-blue-900 ">
+          <h1 className="text-sm mt-3 font-bold">Arcade (Yearly) 
+          <br />
+          <Link to={``}>
+            <a className="paragraph">
+              Change
+            </a>
+          </Link> </h1> 
+          <p className="mt-3">$9/mo</p>
+        </div>
+        <hr className="h-0.5  bg-gray-300"/>
+        <div>
+            <div className="flex space-x-[14rem]">
+              <p className="text-sm paragraph p-3">
+              Online services
+              </p>
+              <span className="paragraph pt-3.5 text-sm">+$1/mo</span></div>
+              <div className="flex space-x-[13.5rem]"><p className="paragraph p-3">
+              Larger storage
+              </p>
+              <span className="paragraph text-sm pt-2.5">+$2/mo</span></div>
+            </div>
+      </div>
+      <div className="flex space-x-[14rem]">
+        <p className=" paragraph ml-2 mt-3 text-sm">
+              Total per month
+        </p>
+            <span className="text-1xl text-blue-700 font-bold mt-3">+$12/mo</span>
+      </div>
+      <div className="flex mt-[10rem]">
+            <Link to={`/plan`}>
+              <p
+                className="ml-8 paragraph
+                 rounded-md p-3">
+                Go back
+              </p>
+            </Link>
+            <Link to={`/thankyou`}>
+              <button
+                className="ml-[13rem] border
+              bg-blue-800 font-normal
+                rounded-md p-2 hover:bg-blue-300">
+                Confirm
+              </button>
+            </Link>
+          </div>
+      </main>
+    </div>
+    </div>
+  );
+}
--- src/components/thankyou.jsx
+++ src/components/thankyou.jsx
@@ -9,15 +9,15 @@ export default function Thankyou() {
            <div div className='lg:m-5'><Selector /></div>
            <main className='m-[6rem] mt-[11rem] thank-you'>
            <div>
-             <img src={ThankYou} alt='A thank you icon' className='mx-auto' />
+             <img src={ThankYou} alt='A thank you icon' className='mx-auto thankyou-img' />
              <h1 className='text-3xl text-blue-900 font-bold m-6 pl-[6rem]'>
                 Thank you!
              </h1>
-             <p className="paragraph font-normal text-md">
+             <p className="paragraph font-normal text-md thankyou-text">
                 Thanks for confirming your subscription!
                 We hope you have <br /> fun using our platform.
                 If you need any support, please feel <br/>
-               <span className='pl-10'> free to email us at 
+               <span className='pl-10 thankyou-text'> free to email us at 
                support@loremgaming.com </span>
              </p> </div>
            </main>
